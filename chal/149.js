2bd18bb2cee99ebaa269a07b6d2a56c36892e334
m0x0m0x <mx0mx@tuta.io>
2024-03-26 02:45:34 +0530

    149CodeChal

--- chal/149.js
+++ chal/149.js
@@ -5,6 +5,36 @@
 import { mainHead } from "../UTILS/text.js";
 // ===
 mainHead("149: Coding Challenge #1 ");
-console.log("he");
-console.log("he");
-console.log("he");
+
+// Solution
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
+
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult and is ${dog} `);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy and is ${dog}`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// Alternative Solution
+
+// const checkDogs2 = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+
+//   dogsJulia.slice(1, 3);
+
+//   console.log(dogsJuliaCorrected);
+// };
+
+// checkDogs2([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
