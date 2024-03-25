4da0e2a0fb8ea67a8ca625bdc6fa64abb7c045d2
m0x0m0x <mx0mx@tuta.io>
2024-03-26 02:33:24 +0530

    add

---
+++ chal/149.js
@@ -0,0 +1,10 @@
+/* 
+149: Coding Challenge #1 
+*/
+//=== Import Zone ===
+import { mainHead } from "../UTILS/text.js";
+// ===
+mainHead("149: Coding Challenge #1 ");
+console.log("he");
+console.log("he");
+console.log("he");
