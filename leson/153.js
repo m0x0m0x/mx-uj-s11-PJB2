108c9286a707cb4f51a3a38942b584e3f9d24ab0
m0x0m0x <mx0mx@tuta.io>
2024-03-27 02:21:45 +0530

    filter method

--- leson/153.js
+++ leson/153.js
@@ -10,3 +10,27 @@ mainHead("153: Filter Method");
 subHead("Examine Data");
 console.table(movements);
 console.table(currencies);
+
+subHead("Your work copy the array");
+
+console.log(movements);
+
+// Using spread operator to copy the movements
+const mvm = [...movements];
+console.log(mvm);
+
+subHead("Filter Method Work");
+// removing all the negative values
+const deposits = mvm.filter(function (m) {
+  return m > 0;
+});
+
+console.log(`
+Original - ${mvm}
+With Filer  - ${deposits}
+`);
+
+//using for of loop - pushing into array
+const depositFor = [];
+for (const m of mvm) if (m > 0) depositFor.push(m);
+console.log(depositFor);
