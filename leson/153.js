1ca4b78b0d8597c70c83a853b80dfe41b292ad5d
m0x0m0x <mx0mx@tuta.io>
2024-03-27 01:59:35 +0530

    add

---
+++ leson/153.js
@@ -0,0 +1,14 @@
+/* 
+153: Filter Method
+*/
+// === Import Zone
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+import { currencies, movements } from "./bankdata.js";
+//===
+mainHead("153: Filter Method");
+
+subHead("Examine Data");
+console.table(movements);
+console.table(currencies);
+
+console.log("pus");
