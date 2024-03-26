d737b8e01893f133754fc60cdf8b6f4f3a5e6bf2
m0x0m0x <mx0mx@tuta.io>
2024-03-26 23:11:08 +0530

    151

--- leson/151.js
+++ leson/151.js
@@ -2,7 +2,7 @@
 151: Map Method - for arrays 
 */
 // === Import Zone
-import { mainHead, subHead } from "../UTILS/text.js";
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
 import { currencies, movements } from "./bankdata.js";
 //===
 mainHead("151: Map Method - for arrays ");
@@ -10,3 +10,26 @@ mainHead("151: Map Method - for arrays ");
 subHead("Examine Data");
 console.table(movements);
 console.table(currencies);
+
+subHead("Convert Euro to dollar from movements");
+const eu2usd = 1.1;
+const mUSD = movements.map(function (m) {
+  return m * eu2usd;
+  //   return 23;
+});
+
+// New array from the map method
+paraText("Print New Array");
+console.table(mUSD);
+paraText("Print Both movements and mUSD");
+console.log(mUSD, movements);
+
+subHead("Write above method with forOf Loop");
+
+const mUSD2 = [];
+for (const mov of movements) mUSD2.push(mov * eu2usd);
+console.log(mUSD2);
+
+// Write this as an arrow function
+
+const mUSD3 = movements.map((mov) => mov * eu2usd);
