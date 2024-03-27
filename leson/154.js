911071c09e147a6c4342e6d8a0c92d6d662866e2
m0x0m0x <mx0mx@tuta.io>
2024-03-27 21:14:21 +0530

    154

---
+++ leson/154.js
@@ -0,0 +1,9 @@
+/* 
+154: The Reduce method
+*/
+//=== Import Zone
+import { mainHead, subHead } from "../UTILS/text.js";
+import {} from "./bankdata.js";
+//===
+mainHead("154: The Reduce method");
+subHead("Another ARRAY method");
