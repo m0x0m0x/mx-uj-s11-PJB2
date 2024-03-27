e5894f47371bee23c044949f7792f781e1907294
m0x0m0x <mx0mx@tuta.io>
2024-03-27 22:47:50 +0530

    154

--- leson/154.js
+++ leson/154.js
@@ -2,8 +2,35 @@
 154: The Reduce method
 */
 //=== Import Zone
-import { mainHead, subHead } from "../UTILS/text.js";
-import {} from "./bankdata.js";
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+import { currencies, movements } from "./bankdata.js";
 //===
 mainHead("154: The Reduce method");
-subHead("Another ARRAY method");
+
+subHead("Print Imported arrays");
+console.log(movements, currencies);
+
+subHead("Reduce Method - Reduce elemens into one array");
+
+// Copying movements to a new array
+const mvm = [...movements];
+console.log(mvm);
+
+// Now doing the reduction method
+// acc = accumulator variable
+const gloBal = mvm.reduce(function (acc, m, i, a) {
+  console.log(`Itn ${i}: ${acc}`);
+  return acc + m;
+}, 0);
+console.log(gloBal);
+
+// Doing the above with a for loop
+paraText("Doing above with a forloop");
+let bal2 = 0;
+for (const m of mvm) bal2 += m;
+console.log(bal2);
+
+// Writing above with arrow function
+paraText("Reduce function wiht arrow funcion format");
+const global2 = mvm.reduce((a, c, i) => a + c, 0);
+console.log(global2);
