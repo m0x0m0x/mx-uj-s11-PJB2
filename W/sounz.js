4d3d247800544e565fb0373fd755b8815758bcdd
null <null>
2024-04-01 23:42:04 +0530

    added Login Sound

--- W/sounz.js
+++ W/sounz.js
@@ -5,3 +5,9 @@ This is the script for those sounds on button presses
 //////////////////////////////
 // === Login Button Sond ===
 
+const Logbtn = document.querySelector(".login__btn");
+const clickSound = document.getElementById("clickSound");
+Logbtn.addEventListener("click", () => {
+    clickSound.currentTime = 0; // Reset the audio to the beginning
+    clickSound.play(); // Play the click sound
+  });
