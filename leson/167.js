567a403abdae9984f02450c63c36bc6a67559a43
m0x0m0x <mx0mx@tuta.io>
2024-04-03 05:25:02 +0530

    add

--- leson/167.js
+++ leson/167.js
@@ -2,7 +2,20 @@
 165: Array Methods Practice 
 */
 // === import zone
-import { mainHead, subHead } from "../UTILS/text.js";
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+import { accounts } from "./bank2.js";
+import { currencies, movements } from "./bankdata.js";
 //
 
 mainHead("165: Array Methods Practice ");
+
+subHead("Imported data that will be used");
+console.table(movements);
+console.table(currencies);
+console.table(accounts);
+
+subHead("Practice 1");
+
+paraText("Extracting only movements from acc object");
+const bankDepositSum = accounts.map((a) => a.movements);
+console.log(bankDepositSum);
