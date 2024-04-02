52dd7bdb86aa69e76d41c5450c3c4c13c7a15c97
m0x0m0x <mx0mx@tuta.io>
2024-04-02 23:46:16 +0530

    ch 165

---
+++ leson/165.js
@@ -0,0 +1,16 @@
+/*
+165: More Ways of creating and Filling arrays 
+*/
+// === import zone
+import { mainHead, subHead } from "../UTILS/text.js";
+//
+
+mainHead("165: More Ways of creating and Filling arrays ");
+
+subHead("Manual Array Creating");
+
+// Immediate printing
+console.log([1, 2, 3, 4, 5]);
+
+// Using constructor
+console.log(new Array(1, 2, 3, 4, 5));
