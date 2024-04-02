895a50b39f724f0d5449ec038c0d0ca6aaafef9d
m0x0m0x <mx0mx@tuta.io>
2024-04-03 00:12:43 +0530

    c

--- leson/165.js
+++ leson/165.js
@@ -14,3 +14,27 @@ console.log([1, 2, 3, 4, 5]);
 
 // Using constructor
 console.log(new Array(1, 2, 3, 4, 5));
+
+// Programmatically creating arrays
+const n1 = new Array(7);
+console.log(n1);
+
+subHead("Using Fill Method");
+
+// Use map to fill element
+console.log(n1.map(() => 5));
+
+// On empty arrays , you can call the fill method
+
+/// adds multiple elements
+// n1.fill(1);
+
+// beavhior is of slice method with same parameters
+console.log(n1);
+n1.fill(1, 3, 5);
+console.log(n1);
+
+// Tryinbg
+const n2 = [1, 2, 3, 4, 5, 6, 7];
+n2.fill(23, 2, 6);
+console.log(n2);
