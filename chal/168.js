b681e737d833a6fffa22a33f6366e9ce89313d38
m0x0m0x <mx0mx@tuta.io>
2024-04-05 02:35:29 +0530

    168

--- chal/168.js
+++ chal/168.js
@@ -6,4 +6,24 @@ import { mainHead, subHead } from "../UTILS/text.js";
 //====
 mainHead("168: Final Coding challenge for section 11");
 
+subHead("Test Data");
+const dgs = [
+  {
+    weight: 22,
+    curFood: 250,
+    owners: ["Ina"],
+  },
+  {
+    wegiht: 8,
+    curFood: 200,
+    owners: ["Sal", "Min"],
+  },
+  {
+    weight: 32,
+    curFood: 340,
+    owners: ["kin", "lim"],
+  },
+];
+console.table(dgs);
+
 subHead("Challenge 1");
