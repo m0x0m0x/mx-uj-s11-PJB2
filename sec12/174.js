1d1ec7390be61ef82bc5c155c2823f71b90c3dc4
m0x0m0x <mx0mx@tuta.io>
2024-04-08 03:45:58 +0530

    175

---
+++ sec12/174.js
@@ -0,0 +1,27 @@
+/*
+174: Numeric Seperators
+- Formatting numbers
+*/
+//== Imports
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+// ==
+mainHead("174: Numeric Seperators");
+
+subHead("Work");
+// Representing diameter of the solar system which is
+// 30,000,000,000,000 kms
+
+const diaSol = 3_025_698_741_523_658;
+console.log(diaSol);
+
+paraText("Testing with price with seperators");
+const price1 = 345_99;
+console.log(price1);
+
+const trfFee = 15_00;
+
+const PI = 3.14_15_69;
+console.log(PI);
+
+// Converting strings to numbers
+console.log(Number("230_000"));
