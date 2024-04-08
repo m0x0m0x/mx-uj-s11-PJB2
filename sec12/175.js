cc4a7a43e142c125b1fccdeb90e6a7a6d3875824
m0x0m0x <mx0mx@tuta.io>
2024-04-08 22:14:38 +0530

    175

--- sec12/175.js
+++ sec12/175.js
@@ -13,3 +13,22 @@ console.log(Number.MAX_SAFE_INTEGER);
 
 // Testing
 console.log(2 ** 53 + 1);
+console.log(546545644564646464687846545321354878);
+
+paraText("Transforming large numbers to bigint number");
+console.log(546545644564646464687846545321354878n);
+console.log(BigInt(546545644564646464687846545321354878));
+
+subHead("Operationg with BigInt");
+console.log(10000n + 200000n);
+console.log(
+  546545644564646464687846545321354878n * 122389718937198273901739108739107232n
+);
+
+// Cant mix large number multiple with normal int
+let nu1 = 1289371283789217389217389217839712897322817321n;
+let nu2 = 6969;
+// console.log(nu1 * nu2);
+console.log(nu1 * BigInt(nu2));
+
+paraText("Using BigInt for comparison");
