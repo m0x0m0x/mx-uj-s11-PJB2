a4d2cac32af28fb1535e77ce0448a6d5a8ff35b8
m0x0m0x <mx0mx@tuta.io>
2024-04-08 22:03:56 +0530

    175

--- sec12/175.js
+++ sec12/175.js
@@ -6,3 +6,10 @@
 import { mainHead, paraText, subHead } from "../UTILS/text.js";
 // ==
 mainHead("175: Working with BigInt");
+
+paraText("Finding the max value that can be stored in a variable");
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+
+// Testing
+console.log(2 ** 53 + 1);
