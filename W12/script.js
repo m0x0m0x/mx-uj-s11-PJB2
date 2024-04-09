81fa05be4d2bb1c9bdabeac2084a7cd84061d462
m0x0m0x <mx0mx@tuta.io>
2024-04-09 23:18:26 +0530

    178

--- W12/script.js
+++ W12/script.js
@@ -109,13 +109,12 @@ const formatMovementDate = function (date) {
   if (daysPassed === 0) return "Today";
   if (daysPassed === 1) return "Yesterday";
   if (daysPassed <= 7) return `${daysPassed} days ago`;
-  else {
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getUTCFullYear();
 
-    return `${day}/${month}/${year}`;
-  }
+  // THe code here is executed even if else block is not done
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getUTCFullYear();
+  return `${day}/${month}/${year}`;
 };
 
 const displayMovements = function (acc, sort = false) {
