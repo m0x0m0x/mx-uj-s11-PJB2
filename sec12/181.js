371e858b53f724e319058f1b981210fecf52b281
m0x0m0x <mx0mx@tuta.io>
2024-04-10 04:07:46 +0530

    timez

--- sec12/181.js
+++ sec12/181.js
@@ -2,6 +2,24 @@
 181: Timers setTimeout and setInterval
 */
 //=== Import
-import { mainHead } from "../UTILS/text.js";
+import { mainHead, subHead } from "../UTILS/text.js";
 //=== Import
 mainHead("181: Timers setTimeout and setInterval");
+
+subHead("Pizaa Orderin");
+
+// Calling after 3 seconds
+const ingz = ["cuny", "booty"];
+const puzTimer = setTimeout(
+  (ing1, ing2) =>
+    console.log(`
+Lick Pussy 👅 with ${ing1} and ${ing2}
+`),
+  3000,
+  ...ingz
+);
+
+if (ingz.includes("cunny")) clearTimeout(puzTimer);
+
+// Asynchronou JS
+console.log("Wating...");
